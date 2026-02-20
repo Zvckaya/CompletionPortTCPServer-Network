235c7e17dfe6ac06b3ae2c6898071245aacb5409
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2026-02-20 19:20:09 +0900

    초기 커밋.

---
+++ CompletionPortTCPServer/main.cpp
@@ -0,0 +1,103 @@
+#pragma comment(lib,"ws2_32")
+#include <WinSock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+
+struct SOCKETINFO
+{
+	OVERLAPPED overlapped;
+	SOCKET sock;
+	char buf[BUFSIZE + 1];
+	int recvbytes;
+	int sendbytes;
+	WSABUF wsabuf;
+};
+
+DWORD WINAPI WorkerThread(LPVOID arg);
+
+void err_quit(const char* msg);
+void err_display(const char* msg);
+
+int main()
+{
+	int retval;
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+
+	HANDLE hcp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	if (hcp == NULL) return 1;
+
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	HANDLE hThread;
+	for (int i = 0; i < (int)si.dwNumberOfProcessors * 2; i++)
+	{
+		hThread = CreateThread(NULL, 0, WorkerThread, hcp, 0, NULL);
+		if (hThread == NULL) return 1;
+		CloseHandle(hThread);
+	}
+
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("listen");
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	DWORD recvbytes, flags;
+
+	while (1)
+	{
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET)
+		{
+			err_display("accept");
+			break;
+		}
+
+		printf("클라이언트 접속 IP: %s PORT: %d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+		CreateIoCompletionPort((HANDLE)client_sock, hcp, client_sock, 0);
+
+		SOCKETINFO* ptr = new SOCKETINFO;
+		if (ptr == NULL)
+			break;
+		ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
+		ptr->sock = client_sock;
+		ptr->recvbytes = ptr->sendbytes = 0;
+		ptr->wsabuf.buf = ptr->buf;
+		ptr->wsabuf.len = BUFSIZE;
+
+		flags = 0;
+		retval = WSARecv(client_sock, &ptr->wsabuf, 1, &recvbytes, &flags, &ptr->overlapped, NULL);
+		if (retval == SOCKET_ERROR)
+		{
+			if (WSAGetLastError() != ERROR_IO_PENDING)
+			{
+				err_display("WSARecv()");
+			}
+			continue;
+		}
+	}
+
+	WSACleanup();
+	return 0;
+
+}
