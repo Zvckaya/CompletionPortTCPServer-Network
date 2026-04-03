107bd24b0720cfe8ea44bc50b86f602d3030ed6b
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2026-04-03 15:16:45 +0900

    락프리 스택 aba 문제 해결

--- CompletionPortTCPServer/CLFStack.h
+++ CompletionPortTCPServer/CLFStack.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <Windows.h>
+#include "CDebugLog.h"
 
 template<typename T>
 class CLFStack
@@ -10,32 +12,51 @@ public:
 		Node* next;
 	};
 
-
-	CLFStack() :_top(nullptr), _length(0)
-	{
-	};
+	CLFStack() : _top(0), _length(0) {}
 
 	~CLFStack()
 	{
-		while (_top != nullptr)
+		Node* cur = UnpackPtr((UINT64)_top);
+		while (cur != nullptr)
 		{
-			Node* temp = _top;
-			_top = temp->next;
+			Node* temp = cur;
+			cur = cur->next;
 			delete temp;
 		}
-	};
+	}
 
 	CLFStack(const CLFStack&) = delete;
 	CLFStack& operator=(const CLFStack&) = delete;
 
-	int GetLength();
+	int  GetLength();
 	void Push(const T& data);
 	bool Pop(T& data);
 
 private:
-	__declspec(align(8)) Node* volatile _top;
-	volatile int _length;
+	static constexpr UINT64 ADDR_MASK   = 0x00007FFFFFFFFFFF;  
+	static constexpr int    STAMP_SHIFT = 47;
+	static constexpr UINT64 STAMP_MASK  = 0x1FFFF;             
 
+	static LONGLONG Pack(Node* ptr, UINT64 stamp)
+	{
+		return (LONGLONG)(
+			((stamp & STAMP_MASK) << STAMP_SHIFT) |
+			(reinterpret_cast<UINT64>(ptr) & ADDR_MASK)
+		);
+	}
+
+	static Node* UnpackPtr(UINT64 packed)
+	{
+		return reinterpret_cast<Node*>(packed & ADDR_MASK);
+	}
+
+	static UINT64 UnpackStamp(UINT64 packed)
+	{
+		return (packed >> STAMP_SHIFT) & STAMP_MASK;
+	}
+
+	__declspec(align(8)) volatile LONGLONG _top;   // packed: [17-bit stamp | 47-bit ptr]
+	volatile int _length;
 };
 
 template<typename T>
@@ -49,48 +70,51 @@ void CLFStack<T>::Push(const T& data)
 {
 	Node* node = new Node;
 	node->data = data;
-	//Log_Record(eLogEvent::PUSH_NEW, node);                        // new 할당
+	Log_Record(eLogEvent::PUSH_NEW, node);
 
-	Node* oldTop;
+	LONGLONG oldPacked;
+	LONGLONG newPacked;
 	do {
-		oldTop = _top;
+		oldPacked  = _top;
+		Node*  oldTop = UnpackPtr((UINT64)oldPacked);
+		UINT64 stamp  = UnpackStamp((UINT64)oldPacked);
 		node->next = oldTop;
-		//Log_Record(eLogEvent::PUSH_CAS_BEFORE, oldTop, node);    // CAS 직전
-	} while (InterlockedCompareExchangePointer((PVOID volatile*)&_top, node, oldTop) != oldTop);
+		newPacked  = Pack(node, stamp + 1);
+		//Log_Record(eLogEvent::PUSH_CAS_BEFORE, oldTop, node);
+	} while (InterlockedCompareExchange64(&_top, newPacked, oldPacked) != oldPacked);
 
-	Log_Record(eLogEvent::PUSH_CAS_OK, node, node->next);        // CAS 성공: new top=node, next=oldTop
+	//Log_Record(eLogEvent::PUSH_CAS_OK, node, node->next);
 
-	//InterlockedIncrement((volatile LONG*)&_length);
+	InterlockedIncrement((volatile LONG*)&_length);
 }
 
 template<typename T>
 bool CLFStack<T>::Pop(T& data)
 {
-	Node* oldtop;
-	Node* newTop;
+	LONGLONG oldPacked;
+	Node*    oldTop;
+	Node*    newTop;
 	do
 	{
-		oldtop = _top;
-		if (oldtop == nullptr)
+		oldPacked    = _top;
+		oldTop       = UnpackPtr((UINT64)oldPacked);
+		if (oldTop == nullptr)
 			return false;
 
-		newTop = oldtop->next;
-		Log_Record(eLogEvent::POP_CAS_BEFORE, oldtop, newTop);  // CAS 직전
-	} while (InterlockedCompareExchangePointer((PVOID volatile*)&_top, newTop, oldtop) != oldtop);
+		newTop        = oldTop->next;
+		UINT64 stamp  = UnpackStamp((UINT64)oldPacked);
+		LONGLONG newPacked = Pack(newTop, stamp + 1);
+		Log_Record(eLogEvent::POP_CAS_BEFORE, oldTop, newTop);
+	} while (InterlockedCompareExchange64(&_top, newPacked, oldPacked) != oldPacked);
 
-	Log_Record(eLogEvent::POP_CAS_OK, newTop, oldtop);          // CAS 성공: new top=old->next
+	//Log_Record(eLogEvent::POP_CAS_OK, newTop, oldTop);
 
-	data = oldtop->data;
+	data = oldTop->data;
 
-	//Log_Record(eLogEvent::POP_DELETE, oldtop);                  // delete 직전
-	delete oldtop;
+	//Log_Record(eLogEvent::POP_DELETE, oldTop);
+	delete oldTop;
 
-	//InterlockedDecrement((volatile LONG*)&_length);
+	InterlockedDecrement((volatile LONG*)&_length);
 
 	return true;
 }
-
-
-
-
-
